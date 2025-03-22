98b45579b8b3771ab93ac2a78abf732868a52598
LiangshouX <xiaoliangshou.bit@gmail.com>
2025-03-23 00:49:39 +0800

    exp-2

---
+++ moba/dev.py
@@ -0,0 +1,269 @@
+import random
+
+import torch
+from functools import lru_cache
+from einops import rearrange
+
+
+@lru_cache(maxsize=16)
+def calc_chunks(cu_seqlen, moba_chunk_size):
+    """calc chunks that needs moba attention"""
+
+    # batch_sizes[batch_idx] = batch size ( seqlen ) of batch idx
+    batch_sizes = cu_seqlen[1:] - cu_seqlen[:-1]
+    # batch_num_chunk[batch_idx] = how many chunk in batch idx
+    batch_num_chunk = (batch_sizes + (moba_chunk_size - 1)) // moba_chunk_size
+    # cu_num_chunk[batch_idx] = first chunk id of this batch
+    cu_num_chunk = torch.ones(
+        batch_num_chunk.numel() + 1,
+        device=cu_seqlen.device,
+        dtype=batch_num_chunk.dtype,
+    )
+    cu_num_chunk[1:] = batch_num_chunk.cumsum(dim=0)
+    # total chunk ( for all batch )
+    num_chunk = cu_num_chunk[-1]
+    # chunk_sizes[chunk_idx] = chunk_size of chunk idx
+    chunk_sizes = torch.full(
+        (num_chunk + 1,), moba_chunk_size, dtype=torch.int32, device=cu_seqlen.device
+    )
+    chunk_sizes[0] = 0  # for calc cu chunk
+    batch_last_chunk_size = batch_sizes - (batch_num_chunk - 1) * moba_chunk_size
+    chunk_sizes[cu_num_chunk[1:]] = batch_last_chunk_size
+    # cu_chunk[chunk_idx] = the start chunk offset of chunk idx
+    cu_chunk = chunk_sizes.cumsum(dim=-1, dtype=torch.int32)
+    # chunk_to_batch[chunk_idx] = batch idx of the chunk idx
+    chunk_to_batch = torch.zeros(
+        (num_chunk,), dtype=torch.int32, device=cu_seqlen.device
+    )
+    chunk_to_batch[cu_num_chunk[1:-1]] = 1
+    chunk_to_batch = chunk_to_batch.cumsum(dim=0, dtype=torch.int32)
+
+    """ filter chunks that need moba attn """
+
+    # filter chunks ( remove last chunk of each batch )
+    # filtered_chunk_indices: chunk index list that excludes the last chunk of each batch
+    chunk_to_remove = cu_num_chunk[1:] - 1
+    chunk_to_remain = torch.ones(
+        (num_chunk,), dtype=torch.bool, device=cu_seqlen.device
+    )
+    chunk_to_remain[chunk_to_remove] = False
+    filtered_chunk_indices = chunk_to_remain.nonzero(as_tuple=True)[0]
+    num_filtered_chunk = len(filtered_chunk_indices)
+
+    return (
+        cu_chunk,
+        filtered_chunk_indices,
+        num_filtered_chunk,
+        chunk_to_batch,
+    )
+
+
+def moba_attn_varlen(
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        cu_seqlens: torch.Tensor,
+        max_seqlen: int,
+        moba_chunk_size: int,
+        moba_topk: int,
+) -> torch.Tensor:
+    """An efficient version of moba implementation with triton kernels and flash-attn, the core logic:
+    1. Calculate the chunks and the number of chunks, n = floor(data_size / chunk_size)
+       - tokens in the tail chunk are reserved for self attn
+       - tokens in other chunks will be processed in later steps
+    2. K in each chunk will calculate mean value as the representative k, and Q will attend to these representative
+    k to get the gate logit, which will be used to select topk chunks
+    3. Select the topk chunks and get the dense q for each kv chunk pair and do the varlen attention
+    4. Combine the varlen attn and self attn results via online softmax to get the final result
+
+    Args:
+        q (torch.Tensor): [seqlen, head, head_dim]
+        k (torch.Tensor): [seqlen, head, head_dim]
+        v (torch.Tensor): [seqlen, head, head_dim]
+        cu_seqlens (torch.Tensor): the cumulative sequence length tensor, same definition in flash attn
+        max_seqlen (int): the max sequence length of the batch, same definition in flash attn
+
+    Returns:
+        attn_output (torch.Tensor): [seqlen, head, head_dim]
+    """
+
+    kv = torch.stack((k, v), dim=1)
+
+    """ some basic variables """
+    # qkv shape = [ S, H, D ]
+    seqlen, num_head, head_dim = q.shape
+
+    """ prepare chunk meta """
+    (
+        cu_chunk,
+        filtered_chunk_indices,
+        num_filtered_chunk,
+        chunk_to_batch,
+    ) = calc_chunks(cu_seqlens, moba_chunk_size)
+
+    # we will adjust selective topk to moba_topk - 1, as the last chunk is always chosen
+    moba_topk = min(moba_topk - 1, num_filtered_chunk)
+    need_moba_attn = moba_topk > 0
+
+    # corner case: if no moba attn needed, just return self attn
+    if not need_moba_attn:
+        # return flash_attn_varlen_func(
+        #     q, k, v, cu_seqlens, cu_seqlens, max_seqlen, max_seqlen, causal=True
+        # )
+        return q
+
+    self_attn_cu_seqlen = cu_chunk
+
+    # filtered_kv is a dense matrix that only contains filtered chunk of kv
+    filtered_kv_indices = torch.arange(
+        0, moba_chunk_size, dtype=torch.int32, device=q.device
+    )[None, :].repeat(num_filtered_chunk, 1)
+    filtered_kv_indices += cu_chunk[filtered_chunk_indices][:, None]
+    filtered_kv = kv.index_select(0, filtered_kv_indices.view(-1))
+
+    """ calc key_gate_weight and gate """
+
+    # key_gate_weight [ F_N_CHUNK, HEAD, HEAD_DIM ]
+    key_gate_weight = (
+        filtered_kv[:, 0]
+        .view(num_filtered_chunk, moba_chunk_size, num_head, head_dim)
+        .mean(dim=1)
+        .float()
+    )
+    q = q.type(torch.float32)  # float logit on the fly for better gate logit perception
+    key_gate_weight = key_gate_weight.type(
+        torch.float32
+    )  # float logit for better gate logit perception
+    gate = torch.einsum(
+        "nhd,shd->nhs", key_gate_weight, q
+    )  # gate [ F_N_CHUNK, HEAD, SEQ ]
+    key_gate_weight = key_gate_weight.type_as(k)
+    q = q.type_as(k)
+
+    # pose process gate, masking unchosen batch and apply causal mask to current chunk
+    gate_seq_idx = torch.arange(0, seqlen, device=q.device, dtype=torch.int32)[
+                   None, :
+                   ].repeat(num_filtered_chunk, 1)
+    chunk_end = cu_chunk[filtered_chunk_indices + 1]
+    batch_end = cu_seqlens[chunk_to_batch[filtered_chunk_indices] + 1]
+    gate_chunk_end_mask = gate_seq_idx < chunk_end[:, None]
+    gate_batch_end_mask = gate_seq_idx >= batch_end[:, None]
+    gate_inf_mask = gate_chunk_end_mask | gate_batch_end_mask
+    gate.masked_fill_(gate_inf_mask.unsqueeze(1), -float("inf"))
+
+    """ find moba q that needs moba attn """
+    # find topk chunks
+    # gate_mask [ N_CHUNK, HEAD, SEQ ], true indicates that needs attention
+    _, gate_top_k_idx = torch.topk(gate, k=moba_topk, dim=0, largest=True, sorted=False)
+    # apply causal mask
+    gate_mask = torch.logical_not(gate.isinf())
+    # select topk chunks
+    gate_idx_mask = torch.zeros(gate_mask.shape, dtype=torch.bool, device=q.device)
+    gate_idx_mask = gate_idx_mask.scatter_(dim=0, index=gate_top_k_idx, value=True)
+    gate_mask = torch.logical_and(gate_mask, gate_idx_mask)
+
+    # varlen trick: combining all q index that needs moba attn
+    # the result will be like [ C0H0 ][ C0H1 ][ C0H2 ][ ... ][ CnHm ]
+    moba_q_indices = gate_mask.reshape(gate_mask.shape[0], -1).nonzero(as_tuple=True)[
+        -1
+    ]  # [ HS indices ] * N
+    # moba_seqlen_q indicates that how many q chunks are selected for each kv chunk - head
+    moba_seqlen_q = gate_mask.sum(dim=-1).flatten()
+    # select all q that needs moba attn based on the moba_q_indices
+    moba_q = rearrange(q, "s h d -> ( h s ) d").index_select(
+        0, moba_q_indices
+    )  # [ selected_S, D ]
+    moba_q = moba_q.unsqueeze(1)
+    # moba_q_sh_indices represents the position in the origin q tensor of each q token inside moba_q
+    moba_q_sh_indices = moba_q_indices % seqlen * num_head + moba_q_indices // seqlen
+
+    """ prepare moba kv """
+    # Since moba_q is organized as HS * N, we need to reorganize kv to adapt to q
+
+    # cut off zero experts
+    q_zero_mask = moba_seqlen_q == 0
+    valid_expert_mask = ~q_zero_mask
+    zero_expert_count = q_zero_mask.sum()
+    # only keep the kv that has q select > 0
+    if zero_expert_count > 0:
+        moba_seqlen_q = moba_seqlen_q[valid_expert_mask]
+    # moba cu_seqlen for flash attn
+    moba_cu_seqlen_q = torch.cat(
+        (
+            torch.tensor([0], device=q.device, dtype=moba_seqlen_q.dtype),
+            moba_seqlen_q.cumsum(dim=0),
+        ),
+        dim=0,
+    ).to(torch.int32)
+    moba_kv = rearrange(filtered_kv, "s x h d -> h s x d")
+    moba_kv = moba_kv.split(moba_chunk_size, dim=1)
+    moba_kv = torch.cat(moba_kv, dim=0)
+    if zero_expert_count > 0:
+        assert valid_expert_mask.sum() == moba_kv.shape[0] - zero_expert_count
+        moba_kv = moba_kv[
+            valid_expert_mask
+        ]  # cut off zero Q expert from kv , or the grad may be nan
+    moba_kv = moba_kv.flatten(start_dim=0, end_dim=1).unsqueeze(2)
+    moba_cu_seqlen_kv = (
+            torch.arange(
+                0,
+                num_filtered_chunk * num_head + 1 - zero_expert_count,
+                dtype=torch.int32,
+                device=q.device,
+            )
+            * moba_chunk_size
+    )
+
+    # Shape check
+    assert (
+            moba_cu_seqlen_kv.shape == moba_cu_seqlen_q.shape
+    ), f"moba_cu_seqlen_kv.shape != moba_cu_seqlen_q.shape {moba_cu_seqlen_kv.shape} != {moba_cu_seqlen_q.shape}"
+
+
+def generate_data(batch, seqlen, num_q_head, num_kv_head, headdim, dtype):
+    random.seed(0)
+    torch.manual_seed(0)
+    # torch.cuda.manual_seed(0)
+    device = torch.device("cpu")
+
+    # gen qkv
+    q = torch.randn(
+        (seqlen, num_q_head, headdim), dtype=dtype, device=device, requires_grad=True
+    )
+    k = torch.randn(
+        (seqlen, num_kv_head, headdim), dtype=dtype, device=device, requires_grad=True
+    )
+    v = torch.randn(
+        (seqlen, num_kv_head, headdim), dtype=dtype, device=device, requires_grad=True
+    )
+
+    # gen cu seqlen
+    cu_seqlen = random.sample(range(1, seqlen - 1), batch - 1) if batch > 1 else []
+    cu_seqlen.sort()
+    cu_seqlen = [0] + cu_seqlen + [seqlen]
+    cu_seqlen = torch.tensor(cu_seqlen, device=device, dtype=torch.int32)
+
+    # max_seqlen
+    max_seqlen = torch.amax(cu_seqlen[1:] - cu_seqlen[:-1])
+
+    return q, k, v, cu_seqlen, max_seqlen.item()
+
+
+if __name__ == '__main__':
+    batch, head, seqlen, head_dim, moba_chunk_size, moba_topk = 1, 1, 512, 128, 64, 2
+
+    q, k, v, cu_seqlen, max_seqlen = generate_data(
+        batch, seqlen, head, head, head_dim, torch.bfloat16
+    )
+
+    o = moba_attn_varlen(
+        q,
+        k,
+        v,
+        cu_seqlen,
+        max_seqlen,
+        moba_chunk_size=moba_chunk_size,
+        moba_topk=moba_topk,
+    )
+
+    print(0)
--- moba/moba_efficient.py
+++ moba/moba_efficient.py
@@ -1,12 +1,13 @@
 """A clean version of efficient moba implementation with flash-attn on Mindspore"""
+import random
+from functools import lru_cache
 
 import mindspore as ms
-from functools import lru_cache
 from einops import rearrange
-from mindspore.ops import zeros
-from mindspore import nn, ops, Tensor
-from mindspore.ops.auto_generate import FlashAttentionScoreGrad, FlashAttentionScore
-from mindspore.ops.function.grad.grad_func import dtype
+from mindformers.modules.flash_attention import FlashAttention
+from mindspore import nn, ops
+from mindspore.ops import Tensor
+ms.set_context(device_target="CPU")
 
 
 @lru_cache(maxsize=16)
@@ -19,18 +20,18 @@ def calc_chunks(cu_seqlen, moba_chunk_size):
     batch_num_chunk = (batch_sizes + (moba_chunk_size - 1)) // moba_chunk_size
 
     # 初始化累积块数量数组（初始全为1，后续用累积和填充）
-    cu_num_chunk = ms.mint.ones(
+    cu_num_chunk = ops.ones(
         batch_num_chunk.numel() + 1,
         dtype=batch_num_chunk.dtype
     )
     # 从第二个元素开始填入累积块数（例如：[1, 1+2, 1+2+3, ...]）
-    cu_num_chunk[1:] = batch_num_chunk.cumsum(dim=0)
+    cu_num_chunk[1:] = batch_num_chunk.cumsum(axis=0)
 
     # 总块数（所有批次块数总和）
-    num_chunk = cu_num_chunk[-1].item()
+    num_chunk = cu_num_chunk[-1]
 
     # 初始化所有块的大小为指定块大小（最后会修正最后一块的大小）
-    chunk_sizes = ms.mint.full(
+    chunk_sizes = ops.full(
         (num_chunk + 1,),
         moba_chunk_size,
         dtype=ms.int32,
@@ -43,28 +44,26 @@ def calc_chunks(cu_seqlen, moba_chunk_size):
     chunk_sizes[cu_num_chunk[1:]] = batch_last_chunk_size
 
     # 计算每个块的起始位置（累积块大小）
-    cu_chunk = chunk_sizes.cumsum(dim=-1, dtype=ms.int32)
+    cu_chunk = chunk_sizes.cumsum(axis=-1, dtype=ms.int32)
 
     # 初始化块到批次的映射数组（初始全为0）
-    chunk_to_batch = ms.mint, zeros(
-        (num_chunk,),
+    chunk_to_batch = ops.zeros(
+        num_chunk,
         dtype=ms.int32
     )
+    chunk_to_batch[cu_num_chunk[0:-1]] = 1
+    chunk_to_batch = chunk_to_batch.cumsum(axis=0, dtype=ms.int32)
 
-    chunk_to_batch = ms.mint.zeros(
-        (num_chunk,),
-        dtype=ms.int32
-    )
-    chunk_to_batch[cu_num_chunk[1:-1]] = 1
-    chunk_to_batch = chunk_to_batch.cumsum(dim=0, dtype=ms.int32)
-
+    # 移除每个批次的最后一个块，生成需要处理的块索引
     chunk_to_remove = cu_num_chunk[1:] - 1
-    chunk_to_remain = ms.mint.ones(
-        (num_chunk,),
+    chunk_to_remain = ops.ones(
+        num_chunk,
         dtype=ms.bool_
     )
     chunk_to_remain[chunk_to_remove] = False
-    filtered_chunk_indices = chunk_to_remain.nonzero(as_tuple=True)[0]
+    indices = ops.nonzero(chunk_to_remain)
+    filtered_chunk_indices = indices[:, 0]
+    # filtered_chunk_indices = ops.nonzero(chunk_to_remain)[0]
     num_filtered_chunk = len(filtered_chunk_indices)
 
     return (
@@ -74,12 +73,13 @@ def calc_chunks(cu_seqlen, moba_chunk_size):
         chunk_to_batch,
     )
 
+
 class MixedAttention(nn.Cell):
     def __init__(self, max_seqlen, moba_chunk_size):
         super().__init__()
         self.max_seqlen = max_seqlen
         self.moba_chunk_size = moba_chunk_size
-        self.flash_attn = FlashAttentionScore()
+        self.flash_attn = FlashAttention()
 
         # 初始化其他算子
         self.exp = ops.Exp()
@@ -97,34 +97,39 @@ class MixedAttention(nn.Cell):
                   moba_cu_seqlen_q, moba_cu_seqlen_kv,  # [B_moba + 1]
                   moba_q_sh_indices,  # 索引张量
                   ):
+        # 计算 softmax_scale，倒数平方根，缩放注意力分数，避免数值过大导致 softmax 梯度消失或爆炸
         softmax_scale = q.shape[-1] ** (-0.5)
 
         # ------------------------------------------------------------------------------------------------------------------ #
-        #                                        Self Attention 分支
+        #                                       Self Attention
         # ------------------------------------------------------------------------------------------------------------------ #
-        self_attn_out, self_attn_lse_hs = self.flash_attn(
+        self_attn_out_sh, self_attn_lse_hs = self.flash_attn(
             q, k, v,
 
         )
-        # LSE 形状转换  [H,S] -> [S,H]
-        self_attn_lse_sh = self.transpose(self_attn_lse_hs, (1, 0))
 
         # ------------------------------------------------------------------------------------------------------------------ #
-        #                                        MoBA Attention 分支
+        #                                        MoBA Attention
         # ------------------------------------------------------------------------------------------------------------------ #
         moba_k, moba_v = moba_kv[:, 0], moba_kv[:, 1]
         moba_attn_out, moba_attn_lse_hs = self.flash_attn(
-            query=q, key=k, value=v,
+            query=moba_q, key=moba_k, value=moba_v,
             drop_mask=None,
             actual_seq_qlen=moba_cu_seqlen_q,
         )
-        moba_attn_lse = self.transpose(moba_attn_lse_hs, (1, 0))
 
         # ------------------------------------------------------------------------------------------------------------------ #
-        #                                              混合逻辑
+        #                                        LSE 形状转换  [H,S] -> [S,H]
         # ------------------------------------------------------------------------------------------------------------------ #
-        output = self.zeros((q.shape[0], q.shape[1], q.shape[2]), q.dtype)
-        output_2d = self.reshape(output, (-1, q.shape[2]))
+        self_attn_lse_sh = self.transpose(self_attn_lse_hs, (1, 0)).contiguous()
+        moba_attn_lse = self.transpose(moba_attn_lse_hs, (1, 0)).contiguous()
+
+        # ------------------------------------------------------------------------------------------------------------------ #
+        #                                 混合逻辑，[S,H,D]全零张量 -> [vS,D]二维张量，方便索引和加权
+        # ------------------------------------------------------------------------------------------------------------------ #
+        output = ops.zeros((q.shape[0], q.shape[1], q.shape[2]), q.dtype)
+        # output_2d = self.reshape(output, (-1, q.shape[2]))
+        output_2d = output.view(-1, q.shape[2])
 
         # ------------------------------------------------------------------------------------------------------------------ #
         #                                           1. 计算混合LSE
@@ -134,6 +139,7 @@ class MixedAttention(nn.Cell):
             0, moba_q_sh_indices, moba_attn_lse.view(-1), "amax"
         )
         self_attn_lse_sh = self_attn_lse_sh - max_lse_1d.view_as(self_attn_lse_sh)
+        # moba_attn_lse = moba_attn_lse - self._gather(max_lse_1d, moba_q_sh_indices)
         moba_attn_lse = (
             moba_attn_lse.view(-1)
             .sub(max_lse_1d.index_select(0, moba_q_sh_indices))
@@ -152,7 +158,7 @@ class MixedAttention(nn.Cell):
         mixed_attn_lse_sh = mixed_attn_se_sh.log()
 
         # ------------------------------------------------------------------------------------------------------------------ #
-        #                                            3. 融合输出
+        #                                           3. Attn输出融合
         # ------------------------------------------------------------------------------------------------------------------ #
         factor = (self_attn_lse_sh - mixed_attn_lse_sh).exp()  # [ vS, H ]
         self_attn_out_sh = self_attn_out_sh * factor.unsqueeze(-1)
@@ -161,13 +167,216 @@ class MixedAttention(nn.Cell):
         # ------------------------------------------------------------------------------------------------------------------ #
         #                                           4. MoBA输出融合
         # ------------------------------------------------------------------------------------------------------------------ #
+        mixed_attn_lse = (
+            mixed_attn_lse_sh.view(-1)
+            .index_select(0, moba_q_sh_indices)
+            .view_as(moba_attn_lse)
+        )
+        factor = (moba_attn_lse - mixed_attn_lse).exp()  # [ vS, H ]
+        moba_attn_out = moba_attn_out * factor.unsqueeze(-1)
+        raw_attn_out = moba_attn_out.view(-1, moba_attn_out.shape[-1])
+        output_2d.index_add_(0, moba_q_sh_indices, raw_attn_out)
+        output = output.to(q.dtype)
 
-    def _index_reduce_amax(self, target, indices, src):
-        # 手动实现类似 pytorch 的index_reduce(amax)
-        gathered = self._gather(target, indices, 0)
+        mixed_attn_lse_sh = mixed_attn_lse_sh + max_lse_1d.view_as(mixed_attn_se_sh)
 
-    def _gather(self, tensor, indices):
-        return ops.Gather()(tensor, indices, 0)
+        return output
 
-    def _index_add(self, target, indices, src):
-        return None
+
+def moba_attn_varlen(
+        q: Tensor,
+        k: Tensor,
+        v: Tensor,
+        cu_seqlens: Tensor,
+        max_seqlen: int,
+        moba_chunk_size: int,
+        moba_topk: int,
+) -> Tensor:
+    kv = ops.stack((k, v), axis=1)
+
+    """ Basic variables """
+    # qkv shape = [ S, H, D ]
+    seqlen, num_head, head_dim = q.shape
+
+    """ Prepare chunk meta """
+    (
+        cu_chunk,
+        filtered_chunk_indices,
+        num_filtered_chunk,
+        chunk_to_batch,
+    ) = calc_chunks(cu_seqlens, moba_chunk_size)
+
+    # we will adjust selective topk to moba_topk - 1, as the last chunk is always chosen
+    moba_topk = min(moba_topk - 1, num_filtered_chunk)
+    need_moba_attn = moba_topk > 0
+
+    # corner case: if no moba attn needed, just return self attn
+    if not need_moba_attn:
+        # return flash_attn_varlen_func(
+        #     q, k, v, cu_seqlens, cu_seqlens, max_seqlen, max_seqlen, causal=True
+        # )
+        return q
+
+    self_attn_cu_seqlen = cu_chunk
+
+    """ Prepare filtered KV """
+    # filtered_kv is a dense matrix that only contains filtered chunk of kv
+    # todo 这一步有问题，torch是[7,64]   ms是[7,448]
+    filtered_kv_indices = ops.arange(
+        0, moba_chunk_size, dtype=ms.int32
+    )[None, :].repeat(num_filtered_chunk, 0)
+    filtered_kv_indices += cu_chunk[filtered_chunk_indices][:, None]
+    filtered_kv = kv.index_select(0, filtered_kv_indices.view(-1))
+
+    """ Calculate gate weights """
+    # Convert to float32 for computation
+    key_gate_weight = (filtered_kv[:, 0]
+                       .view(num_filtered_chunk, moba_chunk_size, num_head, head_dim)
+                       .mean(axis=1)
+                       .astype(ms.float32))
+    q = q.astype(ms.float32)
+
+    # Einsum operation
+    gate = ops.einsum("nhd,shd->nhs", key_gate_weight, q)
+    key_gate_weight = key_gate_weight.astype(k.dtype)
+    q = q.astype(k.dtype)
+
+    """ Apply masks """
+    # Create sequence indices
+    gate_seq_idx = (ops.arange(0, seqlen, dtype=ms.int32)[
+                    None, :
+                    ].repeat(num_filtered_chunk, 0))
+
+    chunk_end = cu_chunk[filtered_chunk_indices + 1]
+    batch_end = cu_seqlens[chunk_to_batch[filtered_chunk_indices] + 1]
+    gate_chunk_end_mask = gate_seq_idx < chunk_end[:, None]
+    gate_batch_end_mask = gate_seq_idx >= batch_end[:, None]
+    gate_inf_mask = gate_chunk_end_mask | gate_batch_end_mask
+    # todo 这一步会报错 RuntimeError: For 'Gather', the 'input_indices' should be in the range [0, 2), but got 2, error_code[58]
+    gate.masked_fill(gate_inf_mask.unsqueeze(1), -float("inf"))
+    # Apply mask using full-like tensor and where
+    # gate = ops.masked_fill(gate, gate_inf_mask.expand_dims(1), -float('inf'))
+
+    """ Find topk chunks """
+    # Topk operation
+    _, gate_top_k_idx = ops.topk(gate, moba_topk, dim=0, largest=True, sorted=False)
+    gate_mask = ops.logical_not(ops.isinf(gate))
+
+    # Create scatter mask
+    gate_idx_mask = ops.zeros(gate_mask.shape, ms.bool_)
+    gate_idx_mask = ops.scatter(gate_idx_mask, 0, gate_top_k_idx, ops.ones_like(gate_idx_mask))
+    gate_mask = ops.logical_and(gate_mask, gate_idx_mask)
+
+    """ Process MOBA Q indices """
+    # Reshape and find indices
+    moba_q_indices = ops.nonzero(gate_mask.reshape(gate_mask.shape[0], -1))[..., -1]
+    moba_seqlen_q = ops.reduce_sum(gate_mask.astype(ms.int32), axis=-1).flatten()
+
+    # Rearrange Q
+    moba_q = rearrange(q, "s h d -> ( h s ) d").index_select(
+        0, moba_q_indices
+    )  # [ selected_S, D ]
+    moba_q = moba_q.unsqueeze(1)
+    # moba_q_sh_indices represents the position in the origin q tensor of each q token inside moba_q
+    moba_q_sh_indices = moba_q_indices % seqlen * num_head + moba_q_indices // seqlen
+
+    """ Prepare MOBA KV """
+    # Process KV chunks
+    q_zero_mask = moba_seqlen_q == 0
+    valid_expert_mask = ~q_zero_mask
+    zero_expert_count = q_zero_mask.sum()
+
+    if zero_expert_count > 0:
+        moba_seqlen_q = moba_seqlen_q[valid_expert_mask]
+
+    moba_cu_seqlen_q = ops.concat((
+        Tensor([0], dtype=ms.int32),
+        ops.cumsum(moba_seqlen_q, axis=0)
+    )).astype(ms.int32)
+
+    # Reshape and split KV
+    moba_kv = rearrange(filtered_kv, "s x h d -> h s x d")
+    moba_kv = moba_kv.split(moba_chunk_size, dim=1)
+    moba_kv = ops.cat(moba_kv, axis=0)
+
+    if zero_expert_count > 0:
+        assert valid_expert_mask.sum() == moba_kv.shape[0] - zero_expert_count
+        moba_kv = moba_kv[
+            valid_expert_mask
+        ]  # cut off zero Q expert from kv , or the grad may be nan
+    moba_kv = moba_kv.flatten(start_dim=0, end_dim=1).unsqueeze(2)
+    moba_cu_seqlen_kv = (
+            ops.arange(
+                0,
+                num_filtered_chunk * num_head + 1 - zero_expert_count,
+                dtype=ms.int32,
+            )
+            * moba_chunk_size
+    )
+
+    # Shape check
+    assert (
+            moba_cu_seqlen_kv.shape == moba_cu_seqlen_q.shape
+    ), f"moba_cu_seqlen_kv.shape != moba_cu_seqlen_q.shape {moba_cu_seqlen_kv.shape} != {moba_cu_seqlen_q.shape}"
+
+    """ Final attention computation """
+    return MixedAttention()(
+        q, k, v,
+        self_attn_cu_seqlen,
+        moba_q, moba_kv,
+        moba_cu_seqlen_q,
+        moba_cu_seqlen_kv,
+        max_seqlen,
+        moba_chunk_size,
+        moba_q_sh_indices
+    )
+
+
+if __name__ == '__main__':
+    batch, head, seqlen, head_dim, moba_chunk_size, moba_topk = 1, 1, 512, 128, 64, 2
+
+
+    def generate_data(batch, seqlen, num_q_head, num_kv_head, headdim, dtype):
+        random.seed(0)
+        ms.manual_seed(0)
+        # torch.cuda.manual_seed(0)
+        # device = torch.cuda.current_device()
+
+        # gen qkv
+        q = ops.randn(
+            (seqlen, num_q_head, headdim), dtype=dtype
+        )
+        k = ops.randn(
+            (seqlen, num_kv_head, headdim), dtype=dtype
+        )
+        v = ops.randn(
+            (seqlen, num_kv_head, headdim), dtype=dtype
+        )
+
+        # gen cu seqlen
+        cu_seqlen = random.sample(range(1, seqlen - 1), batch - 1) if batch > 1 else []
+        cu_seqlen.sort()
+        cu_seqlen = [0] + cu_seqlen + [seqlen]
+        cu_seqlen = ms.tensor(cu_seqlen, dtype=ms.int32)
+
+        # max_seqlen
+        max_seqlen = ops.amax(cu_seqlen[1:] - cu_seqlen[:-1])
+
+        return q, k, v, cu_seqlen, max_seqlen.item()
+
+
+    q, k, v, cu_seqlen, max_seqlen = generate_data(
+        batch, seqlen, head, head, head_dim, ms.float16
+    )
+
+    o = moba_attn_varlen(
+        q,
+        k,
+        v,
+        cu_seqlen,
+        max_seqlen,
+        moba_chunk_size=moba_chunk_size,
+        moba_topk=moba_topk,
+    )
+
+    print(0)
